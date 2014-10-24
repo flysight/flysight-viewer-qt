2c2a36bec9185b21bedbd5591f013a3d4ee7b8a3
Michael Cooper <michael@flysight.ca>
2014-10-24 10:34:31 -0600

    Added basic Google Maps view

--- src/mapview.cpp
+++ src/mapview.cpp
@@ -1,4 +1,8 @@
 #include "mapview.h"
+
+#include <QWebFrame>
+#include <QWebElement>
+
 #include "mainwindow.h"
 
 MapView::MapView(QWidget *parent) :
@@ -16,5 +20,40 @@ QSize MapView::sizeHint() const
 
 void MapView::updateView()
 {
-    // TODO: Update the view
+    double lower = mMainWindow->rangeLower();
+    double upper = mMainWindow->rangeUpper();
+
+    double xMin, xMax;
+    double yMin, yMax;
+
+    bool first = true;
+
+    for (int i = 0; i < mMainWindow->dataSize(); ++i)
+    {
+        const DataPoint &dp = mMainWindow->dataPoint(i);
+
+        if (lower <= dp.t && dp.t <= upper)
+        {
+            if (first)
+            {
+                xMin = xMax = dp.lon;
+                yMin = yMax = dp.lat;
+                first = false;
+            }
+            else
+            {
+                if (dp.lon < xMin) xMin = dp.lon;
+                if (dp.lon > xMax) xMax = dp.lon;
+
+                if (dp.lat < yMin) yMin = dp.lat;
+                if (dp.lat > yMax) yMax = dp.lat;
+            }
+        }
+    }
+
+    QString str =
+            QString("var newLoc = new google.maps.LatLng(%1, %2); ").arg((yMin + yMax) / 2).arg((xMin + xMax) / 2) +
+            QString("map.setCenter(newLoc);");
+
+    page()->currentFrame()->documentElement().evaluateJavaScript(str);
 }
