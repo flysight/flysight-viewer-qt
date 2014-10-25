60ce7470a0da28d6aa18800b6059ce2b32b3c7ef
Michael Cooper <michael@flysight.ca>
2014-10-24 19:03:07 -0600

    Add flight path to map.

--- src/mapview.cpp
+++ src/mapview.cpp
@@ -28,6 +28,8 @@ void MapView::updateView()
 
     bool first = true;
 
+    QString flightPath("var flightPlanCoordinates = [");
+
     for (int i = 0; i < mMainWindow->dataSize(); ++i)
     {
         const DataPoint &dp = mMainWindow->dataPoint(i);
@@ -48,8 +50,24 @@ void MapView::updateView()
                 if (dp.lat < yMin) yMin = dp.lat;
                 if (dp.lat > yMax) yMax = dp.lat;
             }
+
+            flightPath += QString("new google.maps.LatLng(%1, %2),").arg(dp.lat, 0, 'f').arg(dp.lon, 0, 'f');
         }
     }
+
+    flightPath = flightPath.left(flightPath.size() - 1);
+    flightPath += QString("];");
+
+    flightPath += QString("var flightPath = new google.maps.Polyline({");
+    flightPath += QString("  path: flightPlanCoordinates,");
+    flightPath += QString("  geodesic: true,");
+    flightPath += QString("  strokeColor: '#FF0000',");
+    flightPath += QString("  strokeOpacity: 1.0,");
+    flightPath += QString("  strokeWeight: 2");
+    flightPath += QString("});");
+
+    flightPath += QString("flightPath.setMap(map);");
+    page()->currentFrame()->documentElement().evaluateJavaScript(flightPath);
 
     QString str =
             QString("var newLoc = new google.maps.LatLng(%1, %2); ").arg((yMin + yMax) / 2).arg((xMin + xMax) / 2) +
