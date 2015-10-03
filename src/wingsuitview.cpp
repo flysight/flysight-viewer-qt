0953f6f21a2ef3738f603d3efb3e3ef1ee9afd8c
Michael Cooper <michael@flysight.ca>
2015-10-03 14:00:52 -0600

    Complete scoring functionality

--- src/wingsuitview.cpp
+++ src/wingsuitview.cpp
@@ -4,7 +4,9 @@
 #include <QPushButton>
 
 #include "common.h"
+#include "datapoint.h"
 #include "mainwindow.h"
+#include "plotvalue.h"
 
 WingsuitView::WingsuitView(QWidget *parent) :
     QWidget(parent),
@@ -30,9 +32,80 @@ QSize WingsuitView::sizeHint() const
 
 void WingsuitView::updateView()
 {
+    const double bottom = mMainWindow->windowBottom();
+    const double top = mMainWindow->windowTop();
+
     // Update window bounds
-    ui->bottomEdit->setText(QString("%1").arg(mMainWindow->windowBottom()));
-    ui->topEdit->setText(QString("%1").arg(mMainWindow->windowTop()));
+    ui->bottomEdit->setText(QString("%1").arg(bottom));
+    ui->topEdit->setText(QString("%1").arg(top));
+
+    int iBottom, iTop;
+    bool armed = false;
+
+    // Find end of window
+    for (int i = mMainWindow->dataSize() - 1; i >= 0; --i)
+    {
+        const DataPoint &dp = mMainWindow->dataPoint(i);
+
+        if (dp.alt < bottom)
+        {
+            iBottom = i;
+        }
+
+        if (dp.alt < top)
+        {
+            iTop = i;
+        }
+        else
+        {
+            armed = true;
+        }
+
+        if (armed && DataPoint::energyRate(dp) > 0) break;
+    }
+
+    if (armed)
+    {
+        // Calculate bottom of window
+        const DataPoint &dp1 = mMainWindow->dataPoint(iBottom - 1);
+        const DataPoint &dp2 = mMainWindow->dataPoint(iBottom);
+        DataPoint dpBottom = DataPoint::interpolate(dp1, dp2, (bottom - dp1.alt) / (dp2.alt - dp1.alt));
+
+        // Calculate top of window
+        const DataPoint &dp3 = mMainWindow->dataPoint(iTop - 1);
+        const DataPoint &dp4 = mMainWindow->dataPoint(iTop);
+        DataPoint dpTop = DataPoint::interpolate(dp3, dp4, (top - dp3.alt) / (dp4.alt - dp3.alt));
+
+        // Calculate results
+        const double time = dpBottom.t - dpTop.t;
+        const double distance = mMainWindow->getDistance(dpBottom, dpTop) ;
+        const double speed = distance / time;
+
+        // Update display
+        if (mMainWindow->units() == PlotValue::Metric)
+        {
+            ui->timeEdit->setText(QString("%1").arg(time));
+            ui->distanceEdit->setText(QString("%1").arg(distance / 1000));
+            ui->distanceUnits->setText(tr("km"));
+            ui->speedEdit->setText(QString("%1").arg(speed * MPS_TO_KMH));
+            ui->speedUnits->setText(tr("km/h"));
+        }
+        else
+        {
+            ui->timeEdit->setText(QString("%1").arg(time));
+            ui->distanceEdit->setText(QString("%1").arg(distance * METERS_TO_FEET / 5280));
+            ui->distanceUnits->setText(tr("mi"));
+            ui->speedEdit->setText(QString("%1").arg(speed * MPS_TO_MPH));
+            ui->speedUnits->setText(tr("mph"));
+        }
+    }
+    else
+    {
+        // Update display
+        ui->timeEdit->setText(tr("n/a"));
+        ui->distanceEdit->setText(tr("n/a"));
+        ui->speedEdit->setText(tr("n/a"));
+    }
 }
 
 void WingsuitView::onFAIButtonClicked()
