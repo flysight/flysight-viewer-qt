89a15564ba751160ea1358ef1279bb0aca7ea5a5
Michael Cooper <crwper@gmail.com>
2016-07-25 19:34:43 -0600

    Import from command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    // Import a file if specified on the command line
+    if (QCoreApplication::arguments().size() >= 2)
+    {
+        w.importFile(QCoreApplication::arguments().at(1));
+    }
     
     return a.exec();
 }
