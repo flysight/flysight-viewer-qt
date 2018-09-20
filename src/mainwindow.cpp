68ee73362da86e319c1e864ec2b36072231aa5b0
Michael Cooper <michael@flysight.ca>
2018-09-19 21:36:33 -0600

    Detect exit using vertical acceleration

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1029,7 +1029,7 @@ void MainWindow::import(
     }
 
     // Initialize time
-    initTime(data, trackName, initDatabase);
+    initTime(data);
 
     // Altitude above ground
     initAltitude(data, trackName, initDatabase);
@@ -1037,11 +1037,28 @@ void MainWindow::import(
     // Raw acceleration
     initAcceleration(data);
 
+    // Pick exit
+    initExit(data, trackName, initDatabase);
+
     // Wind adjustments
     updateVelocity(data, trackName, initDatabase);
 }
 
 void MainWindow::initTime(
+        DataPoints &data)
+{
+    const DataPoint &dp0 = data[data.size() - 1];
+    qint64 start = dp0.dateTime.toMSecsSinceEpoch();
+
+    for (int i = 0; i < data.size(); ++i)
+    {
+        DataPoint &dp = data[i];
+        qint64 end = dp.dateTime.toMSecsSinceEpoch();
+        dp.t = (double) (end - start) / 1000;
+    }
+}
+
+void MainWindow::initExit(
         DataPoints &data,
         QString trackName,
         bool initDatabase)
@@ -1055,8 +1072,33 @@ void MainWindow::initTime(
     }
     else
     {
-        const DataPoint &dp0 = data[data.size() - 1];
-        start = dp0.dateTime.toMSecsSinceEpoch();
+        bool foundExit = false;
+
+        for (int i = 1; i < data.size(); ++i)
+        {
+            const DataPoint &dp1 = data[i - 1];
+            const DataPoint &dp2 = data[i];
+
+            double a = (5 - dp1.az) / (dp2.az - dp1.az);
+            if (0 <= a && a < 1)
+            {
+                double vAcc = dp1.vAcc + a * (dp2.vAcc - dp1.vAcc);
+                if (vAcc < 10)
+                {
+                    foundExit = true;
+                    qint64 t1 = dp1.dateTime.toMSecsSinceEpoch();
+                    qint64 t2 = dp2.dateTime.toMSecsSinceEpoch();
+                    start = t1 + a * (t2 - t1);
+                    break;
+                }
+            }
+        }
+
+        if (!foundExit)
+        {
+            const DataPoint &dp0 = data[data.size() - 1];
+            start = dp0.dateTime.toMSecsSinceEpoch();
+        }
     }
 
     if (initDatabase)
