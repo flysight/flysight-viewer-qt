4f50e658f9b4c05acb13d3796c07b36fc2010e5f
Michael Cooper <crwper@gmail.com>
2022-06-21 08:15:34 -0600

    Fix issue in GeographicLib

--- include/GeographicLib/Geoid.hpp
+++ include/GeographicLib/Geoid.hpp
@@ -123,7 +123,7 @@ namespace GeographicLib {
       _file.seekg(
 #if !(defined(__GNUC__) && __GNUC__ < 4)
                   // g++ 3.x doesn't know about the cast to streamoff.
-                  std::ios::streamoff
+                  std::streamoff
 #endif
                   (_datastart +
                    pixel_size_ * (unsigned(iy)*_swidth + unsigned(ix))));
